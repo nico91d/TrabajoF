cfac9b37f29105fcc04779bbec1f0b9f08ac72a0
meliBruve <melibruve@gmail.com>
2020-04-22 21:25:10 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>FUNDACIÓN ARK HO</span></strong>| DESARROLLO @PIXIES
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+</body>
+
+</html>
