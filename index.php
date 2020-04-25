3ad0a784f63e9fa25cc0f6d15281aecdbea634ed
Rolanader20 <64048818+Rolanader20@users.noreply.github.com>
2020-04-25 19:41:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -139,7 +139,7 @@
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
             <div class="icon-box">
               <div class="icon"><i class="bx bxl-dribbble"></i></div>
-              <h4><a href=""></a>Fonoudiología</h4>
+              <h4><a href=""></a>Fonoaudiología</h4>
               <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
             </div>
           </div>
