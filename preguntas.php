ae2a509a6b075018b17f14fd5d9a0c2be7fbf53a
meliBruve <melibruve@gmail.com>
2020-04-25 19:29:51 -0300

    Update preguntas.php

--- preguntas.php
+++ preguntas.php
@@ -1,8 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Fundación Ark Ho</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!--link de los social media en la parte de abajo de la pagina-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
 
 <body>
-<?php include ('menu.php'); ?>
+  <?php include ('conexion.php'); ?>
+  <header id="header">
+    <div class="container d-flex">
+
+      <div class="logo mr-auto">
+        <a href="index.php"><img src="assets/img/menu/logo.png" alt="" class="img-fluid"></a>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="nosotros.php">Nosotros</a></li>
+          <li><a href="inclusion.php">Inclusión Escolar</a></li>
+          <li  class="active"><a href="#">Preguntas Frecuentes</a></li>
+          <li><a href="#">Información Destacada</a></li>
+          <li><a href="contacto.php">Contacto</a></li>
+
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
   <main id="main">
     <!-- ======= Breadcrumbs ======= -->
     <section id="breadcrumbs" class="breadcrumbs">
@@ -22,9 +72,14 @@
     <section id="faq" class="faq">
       <div class="container">
 
+        <div class="section-title" data-aos="fade-up">
+          <h2>PREGUNTAS FRECUENTES</h2>
+        </div>
+
+
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
           <div class="col-lg-5">
-            <i class="ri-question-line"></i>
+            <i class="icofont-question-circle"></i>
             <h4>Non consectetur a erat nam at lectus urna duis?</h4>
           </div>
           <div class="col-lg-7">
@@ -36,7 +91,7 @@
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
           <div class="col-lg-5">
-            <i class="ri-question-line"></i>
+            <i class="icofont-question-circle"></i>
             <h4>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h4>
           </div>
           <div class="col-lg-7">
@@ -48,7 +103,7 @@
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
           <div class="col-lg-5">
-            <i class="ri-question-line"></i>
+            <i class="icofont-question-circle"></i>
             <h4>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?</h4>
           </div>
           <div class="col-lg-7">
@@ -60,7 +115,7 @@
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
           <div class="col-lg-5">
-            <i class="ri-question-line"></i>
+            <i class="icofont-question-circle"></i>
             <h4>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h4>
           </div>
           <div class="col-lg-7">
@@ -72,7 +127,7 @@
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="500">
           <div class="col-lg-5">
-            <i class="ri-question-line"></i>
+            <i class="icofont-question-circle"></i>
             <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
           </div>
           <div class="col-lg-7">
@@ -84,7 +139,7 @@
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="500">
           <div class="col-lg-5">
-            <i class="ri-question-line"></i>
+            <i class="icofont-question-circle"></i>
             <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
           </div>
           <div class="col-lg-7">
@@ -96,7 +151,7 @@
 
         <div class="row faq-item d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="500">
           <div class="col-lg-5">
-            <i class="ri-question-line"></i>
+            <i class="icofont-question-circle"></i>
             <h4>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h4>
           </div>
           <div class="col-lg-7">
@@ -113,6 +168,21 @@
   <?php include ('footer.php'); ?>
 
   <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
 
 </body>
 
